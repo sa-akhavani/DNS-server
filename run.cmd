11aaad65bbffe0792b5e19bf4ad3df10520d052c
Mohammad Noroozi <mnoroozi@outlook.com>
2017-03-06 01:08:00 +0330

    Distributed DNS implemented and tested, catching implemented but not tested

---
+++ run.cmd
@@ -0,0 +1,7 @@
+start java -cp out\artifacts\DNS_server_jar\DNS-server.jar root.RootMain
+start java -cp out\artifacts\DNS_server_jar\DNS-server.jar tld.TLDMain 1
+start java -cp out\artifacts\DNS_server_jar\DNS-server.jar tld.TLDMain 2
+start java -cp out\artifacts\DNS_server_jar\DNS-server.jar authdns.AuthMain 1
+start java -cp out\artifacts\DNS_server_jar\DNS-server.jar authdns.AuthMain 2
+start java -cp out\artifacts\DNS_server_jar\DNS-server.jar authdns.AuthMain 3
+start java -cp out\artifacts\DNS_server_jar\DNS-server.jar authdns.AuthMain 4
